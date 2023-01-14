882780e7092e97acc430e6533253b37d31712a75
altijanamujic <altijana.mujic@edu.fit.ba>
2023-01-14 15:35:30 +0100

    End of section 8

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -3,6 +3,7 @@ using API.Data;
 using API.Interfaces;
 using API.Services;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Sqlite.Design.Internal;
 
 namespace API.Extensions
 {
@@ -17,6 +18,8 @@ namespace API.Extensions
             });
             services.AddCors();
             services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
             return services;
         }
